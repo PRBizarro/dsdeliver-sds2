13fd32e95d3052d03e068df6486031eb7a3d4bae
Your Name <prbizarro@gmail.com>
2021-01-07 17:13:26 -0300

    GET /ORDERS

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
@@ -8,5 +8,5 @@ import com.devsuperior.dsdeliver.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
-	List<Product> findAllByOrderbyNameAsc();
+	List<Product> findAllByOrderByNameAsc();
 }
