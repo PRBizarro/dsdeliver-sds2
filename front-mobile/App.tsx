7e0e5e61eaafe430dcdb39ab395162a5ba8b8586
Your Name <prbizarro@gmail.com>
2021-01-11 15:21:28 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,10 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Header from './src/Header';
 import AppLoading from 'expo-app-loading';
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
-import Home from './src/Home/Index';
+import Routes from './src/Routes';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -17,9 +16,8 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-      <Header />
       <StatusBar style="auto" />
-      <Home />
+      <Routes />
     </View>
   );
 }
